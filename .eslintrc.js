220bd7b62a855d4c31a393c5a8d71474404c23b7
Liberty Code <libertycodeg@gmail.com>
2023-04-02 20:15:17 +0300

    i storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,16 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true,
+    jest: true
   },
-  extends: [
-    "plugin:react/recommended",
-    // "plugin:i18next/recommended",
-    // "plugin:storybook/recommended",
-  ],
+  extends: ["plugin:react/recommended", "plugin:storybook/recommended", "plugin:storybook/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: "latest",
-    sourceType: "module",
+    sourceType: "module"
   },
   plugins: ["react", "@typescript-eslint", "react-hooks", "i18next"],
   rules: {
@@ -23,12 +19,9 @@ module.exports = {
     "react/jsx-indent": [2, 2],
     "react/jsx-indent-props": [2, 2],
     indent: [2, 2],
-    "react/jsx-filename-extension": [
-      2,
-      {
-        extensions: [".js", ".jsx", ".tsx"],
-      },
-    ],
+    "react/jsx-filename-extension": [2, {
+      extensions: [".js", ".jsx", ".tsx"]
+    }],
     "import/no-unresolved": "off",
     "import/prefer-default-export": "off",
     "no-unused-vars": "warn",
@@ -40,39 +33,31 @@ module.exports = {
     "import/extensions": "off",
     "import/no-extraneous-dependencies": "off",
     "no-underscore-dangle": "off",
-    "i18next/no-literal-string": [
-      "error",
-      {
-        markupOnly: true,
-        ignoreAttribute: ["data-testid", "to"],
-      },
-    ],
-    "max-len": [
-      "error",
-      {
-        ignoreComments: true,
-        code: 150,
-      },
-    ],
+    "i18next/no-literal-string": ["error", {
+      markupOnly: true,
+      ignoreAttribute: ["data-testid", "to"]
+    }],
+    "max-len": ["error", {
+      ignoreComments: true,
+      code: 150
+    }],
     "jsx-a11y/no-static-element-interactions": "off",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "error",
     "no-param-reassign": "off",
     "react/display-name": "off",
-    "no-undef": "off",
+    "no-undef": "off"
   },
   globals: {
     __IS_DEV__: true,
     __API__: true,
-    __PROJECT__: true,
+    __PROJECT__: true
   },
-  overrides: [
-    {
-      files: ["**/src/**/*.{test,stories}.{ts,tsx}"],
-      rules: {
-        "i18next/no-literal-string": "off",
-        "max-len": "off",
-      },
-    },
-  ],
+  overrides: [{
+    files: ["**/src/**/*.{test,stories}.{ts,tsx}"],
+    rules: {
+      "i18next/no-literal-string": "off",
+      "max-len": "off"
+    }
+  }]
 };
