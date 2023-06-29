516e425513269a9110843f1df3b1cf5e410bdbf0
Liberty Code <libertycodeg@gmail.com>
2023-06-29 19:57:31 +0300

    change ThemeProvider

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -25,7 +25,9 @@ const ThemeProvider = ({ children, initialTheme }: ThemeProviderProps) => {
       setThemeInited(true);
     }
   }, [defaultTheme, isThemeInited]);
-
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
   const defaultProps = useMemo(
     () => ({
       theme: theme,
